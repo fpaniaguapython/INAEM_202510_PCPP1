8232cd741f7512b8c7173cfb0d9cb5dca74428b6
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2025-10-14 19:24:35 +0200

    Create 04_operador_binario_or.py

---
+++ ejercicios/04_operador_binario_or.py
@@ -0,0 +1,12 @@
+# or 
+# |
+
+# Programar las funciones calcular(), imprimir() y generar()
+# Llamarlas y evaluar si alguna de ellas devuelve True
+# Si True --> Mensaje 'OK'
+# Si no --> Mensaje 'KO'
+# Realizar la evaluación con or y con |
+
+def calcular():
+    print('Calculando...')
+    return False
