738f27ae1372cc4d1b98939e00f3392579b14d8a
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2025-10-15 18:02:29 +0200

    Sesión 02 - Commit parcial

---
+++ ejercicios/10_uso__dict__.py
@@ -0,0 +1,11 @@
+'''
+Crear una clase Pelicula
+Atributos: Título, Director, Año
+Métodos: 
+- agregar_taquilla -> Añadir un nuevo atributo con el importe de la taquilla
+
+1. Crear un película.
+2. Mostrar por pantalla los atributos y los valores (no sabes cuales son)
+3. Agregamos la taquilla
+3. Mostrar por pantalla los atributos y los valores (no sabes cuales son)
+'''
