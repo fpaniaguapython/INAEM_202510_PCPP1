86e32ffb5759279f86aa7dae326ac78362bde826
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2025-10-21 19:27:30 +0200

    Commit parcial

--- ejercicios/29_sistema_persistencia/gestor_fichero_plano.py
+++ ejercicios/29_sistema_persistencia/gestor_fichero_plano.py
@@ -2,8 +2,12 @@ from gestor import GestorPersistenciaBase
 from movie import Movie
 
 class GestorFicheroPlano(GestorPersistenciaBase):
+    @staticmethod
+    def get_nombre_fichero(titulo):
+        return titulo.lower().replace(' ','_')+'.movie'
+
     def create(self, movie : Movie):
-        nombre_fichero = movie.titulo.lower().replace(' ','_')+'.movie'
+        nombre_fichero = GestorFicheroPlano.get_nombre_fichero(movie.titulo)
         with open(nombre_fichero, mode='w', encoding='utf-8') as fichero:
             fichero.write(movie.titulo)
             fichero.write('\n')
@@ -12,7 +16,13 @@ class GestorFicheroPlano(GestorPersistenciaBase):
             fichero.write(str(movie.duracion))
 
     def find_by_title(self, title : str):
-        raise NotImplementedError()
+        nombre_fichero = GestorFicheroPlano.get_nombre_fichero(title)
+        with open(nombre_fichero, mode='r', encoding='utf-8') as fichero:
+            titulo = fichero.readline().replace('\n','')
+            director = fichero.readline().replace('\n','')
+            duracion = fichero.readline().replace('\n','')
+            pelicula = Movie(titulo, director, duracion)
+        return pelicula
 
     def delete(self, title : str):
         raise NotImplementedError()
@@ -21,5 +31,7 @@ class GestorFicheroPlano(GestorPersistenciaBase):
         raise NotImplementedError()
     
 if __name__=='__main__':
-    movie = Movie('El señor de los anillos', 'Peter Jackson', 280)
-    GestorFicheroPlano().create(movie)
+    #movie = Movie('El señor de los anillos', 'Peter Jackson', 280)
+    #GestorFicheroPlano().create(movie)
+    movie = GestorFicheroPlano().find_by_title('El señor de los anillos')
+    print(movie)
--- ejercicios/29_sistema_persistencia/movie.py
+++ ejercicios/29_sistema_persistencia/movie.py
@@ -3,3 +3,6 @@ class Movie:
         self.titulo = titulo
         self.director = director
         self.duracion = duracion
+
+    def __str__(self):
+        return f'Título:{self.titulo}. Director:{self.director}. Duración:{self.duracion}'
