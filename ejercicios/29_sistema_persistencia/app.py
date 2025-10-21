2dbccd471b9706b280f520976e0e525d32f3b08a
Fernando Paniagua <fernando.paniagua.python@gmail.com>
2025-10-21 20:50:17 +0200

    Sesión 05

--- ejercicios/29_sistema_persistencia/app.py
+++ ejercicios/29_sistema_persistencia/app.py
@@ -3,11 +3,11 @@ import movie
 
 if __name__=='__main__':
     mi_gestor = factory.get_sistema_persistencia()
-    #movie = movie.Movie('Halloween', 'John Carpenter', 90)
-    #mi_gestor.create(movie)
+    movie = movie.Movie('Jaws', 'John Carpenter', 90)
+    mi_gestor.create(movie)
     #hw = mi_gestor.find_by_title('Halloween')
     #print(hw)
     #movie = movie.Movie('Halloween', 'John Carpenter y L', 98)
     #mi_gestor.update(movie)
-    mi_gestor.delete('Halloween')
+    #mi_gestor.delete('Halloween')
     
--- ejercicios/29_sistema_persistencia/gestor_configuracion.py
+++ ejercicios/29_sistema_persistencia/gestor_configuracion.py
@@ -1,8 +1,11 @@
 import enum
+import json
 
 class TipoPersistencia(enum.Enum):
     FICHERO_PLANO = 1
     JSON = 2
 
 def get_tipo_persistencia():
-    return TipoPersistencia.JSON
+    with open('config.json',mode='r',encoding='utf-8') as fichero:
+        config = json.load(fichero)
+    return TipoPersistencia[config['tipo_persistencia']]
